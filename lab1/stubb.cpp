a06c602efaa6c0ee6c74215419d408678ea990c7
MichaelSjostrom <micke_moj@hotmail.com>
2016-11-15 11:55:57 +0100

    blablablad

--- lab1/stubb.cpp
+++ lab1/stubb.cpp
@@ -14,7 +14,47 @@
 #include <osg/Group>
 #include <osgUtil/Simplifier>
 #include <osg/LOD>
+#include <osg/MatrixTransform>
+#include <osgUtil/LineSegmentIntersector>
+#include <osgUtil/IntersectionVisitor>
+#include <string>
+#include <iostream>
 
+
+struct MyReadCallback : public osgUtil::IntersectionVisitor::ReadCallback {
+  
+  virtual osg::Node* readNodeFile(const std::string& filename)
+      {
+         std::cout<<"heeej"<<std::endl;
+         return osgDB::readNodeFile(filename);
+      }
+};
+
+
+osg::AnimationPath* createAnimationPath(const osg::Vec3& center,float radius,double looptime){
+
+  osg::AnimationPath* animationPath = new osg::AnimationPath;
+  animationPath->setLoopMode(osg::AnimationPath::LOOP);
+
+  int numSamples = 40;
+  float yaw = 0.0f;
+  float yaw_delta = 2.0f*osg::PI/((float)numSamples-1.0f);
+  float roll = osg::inDegrees(30.0f);
+
+  double time=0.0f;
+  double time_delta = looptime/(double)numSamples;
+
+  for(int i=0;i<numSamples;++i){
+    osg::Vec3 position(center+osg::Vec3(sinf(yaw)*radius,cosf(yaw)*radius,0.0f));
+    osg::Quat rotation(osg::Quat(roll,osg::Vec3(0.0,1.0,0.0))*osg::Quat(-(yaw+osg::inDegrees(90.0f)),osg::Vec3(0.0,0.0,1.0)));
+
+    animationPath->insert(time,osg::AnimationPath::ControlPoint(position,rotation));
+    yaw += yaw_delta;
+    time += time_delta;
+  }
+
+  return animationPath;
+}
 
 
 int main(int argc, char *argv[]){
@@ -23,6 +63,24 @@ int main(int argc, char *argv[]){
 
 #if 1
   osg::ref_ptr<osg::Geode> lineGeode = new osg::Geode();
+
+  //Tripwire
+  osg::Vec3 trip_p0 (-200, 0, 0);
+  osg::Vec3 trip_p1 (200, 0, 0);
+
+  osg::ref_ptr<osg::Vec3Array> verticesTrip = new osg::Vec3Array();
+  verticesTrip->push_back(trip_p0);
+  verticesTrip->push_back(trip_p1);
+
+  osg::ref_ptr<osg::Vec4Array> colorsTrip = new osg::Vec4Array;
+  colorsTrip->push_back(osg::Vec4(0.9f,0.2f,0.3f,1.0f));
+
+  osg::ref_ptr<osg::Geometry> linesGeomTrip = new osg::Geometry();
+  linesGeomTrip->setVertexArray(verticesTrip);
+  linesGeomTrip->setColorArray(colorsTrip, osg::Array::BIND_OVERALL);
+  
+  linesGeomTrip->addPrimitiveSet(new osg::DrawArrays(osg::PrimitiveSet::LINES,0,2));
+
   
   /// Lines for axis ---
   osg::Vec3 x_p0 (0, 0, 0);
@@ -73,6 +131,8 @@ int main(int argc, char *argv[]){
   
   linesGeomZ->addPrimitiveSet(new osg::DrawArrays(osg::PrimitiveSet::LINES,0,2));
 
+  lineGeode->addDrawable(linesGeomTrip);
+
   /*lineGeode->addDrawable(linesGeomX);
   lineGeode->addDrawable(linesGeomY);
   lineGeode->addDrawable(linesGeomZ);*/
@@ -98,7 +158,7 @@ int main(int argc, char *argv[]){
   // create a spot light.
   osg::Light* light1 = new osg::Light;
   light1->setLightNum(0);
-  light1->setPosition(osg::Vec4(60.0f,-20.0f,-2.0f,1.0f));
+  light1->setPosition(osg::Vec4(60.0f,-50.0f,-2.0f,1.0f));
   light1->setAmbient(osg::Vec4(1.0f,1.0f,1.0f,1.0f));
   light1->setDiffuse(osg::Vec4(1.0f,1.0f,1.0f,1.0f));
   light1->setSpotCutoff(80.0f);
@@ -115,7 +175,7 @@ int main(int argc, char *argv[]){
   light2->setLightNum(1);
   light2->setPosition(osg::Vec4(-30.0f,-20.0f,0.0f,1.0f));
   light2->setAmbient(osg::Vec4(1.0f,1.0f,1.0f,1.0f));
-  light2->setDiffuse(osg::Vec4(1.0f,1.0f,1.0f,1.0f));
+  light2->setDiffuse(osg::Vec4(1.0f,0.0f,0.0f,1.0f));
   light2->setSpotCutoff(80.0f);
   light2->setSpotExponent(1.0f);
   light2->setDirection(osg::Vec3(0.0f,1.0f,0.0f));
@@ -139,6 +199,8 @@ int main(int argc, char *argv[]){
    } 
   }
 
+
+
   osg::Image* groundImg = osgDB::readImageFile("ground.jpg");
   osg::Texture2D  *groundTexture = new osg::Texture2D;
   groundTexture->setImage(groundImg);
@@ -146,23 +208,16 @@ int main(int argc, char *argv[]){
   groundTexture->setFilter(osg::Texture2D::MIN_FILTER, osg::Texture2D::LINEAR_MIPMAP_LINEAR);
   groundTexture->setFilter(osg::Texture2D::MAG_FILTER, osg::Texture2D::LINEAR);
   lineGeode->getOrCreateStateSet()->setTextureAttributeAndModes(0, groundTexture);
+
+  //Creating the animationPath
+  osg::Vec3 center(0.0f,0.0f,0.0f);
+  float radius = 100.0f;
+  float animationLength = 10.0f;
+  osg::AnimationPath* animationPath = createAnimationPath(center,radius,animationLength);
   
   osg::ref_ptr<osg::Node> cessnaNode = osgDB::readNodeFile("cessna.osg");
   osg::Node* dumpTruckNode = osgDB::readNodeFile("dumptruck.osg");
-
-  osg::PositionAttitudeTransform* cessnaXform =
-   new osg::PositionAttitudeTransform();
-
-  osg::PositionAttitudeTransform* dumpTruckXform =
-   new osg::PositionAttitudeTransform();
-
-  // Declare and initialize a Vec3 instance to change the
-  // position of the cessna and dumptruck model in the scene
-  osg::Vec3 cessnaPos(60.0,0.0,0.0);
-  cessnaXform->setPosition(cessnaPos);
-
-  osg::Vec3 dumpTruckPos(-70,0.0,0.0);
-  dumpTruckXform->setPosition(dumpTruckPos);
+  osg::ref_ptr<osg::MatrixTransform> cessnaXform;
 
   //Create simplified versions of cessna
   osg::ref_ptr<osg::Node> cessnaNodeLod2 = dynamic_cast<osg::Node*>(cessnaNode->clone( osg::CopyOp::DEEP_COPY_ALL));
@@ -177,10 +232,57 @@ int main(int argc, char *argv[]){
   cessnaNodeLod3->accept(simplifier);
 
   osg::ref_ptr<osg::LOD> lodNode = new osg::LOD();
-  lodNode->addChild(cessnaNode.get(), 0.0f, 10.0f);
-  lodNode->addChild(cessnaNodeLod2.get(), 10.0f, 30.0f);
-  lodNode->addChild(cessnaNodeLod3.get(), 30.0f, FLT_MAX);
-   
+  lodNode->addChild(cessnaNode.get(), 0.0f, 50.0f);
+  lodNode->addChild(cessnaNodeLod2.get(), 50.0f, 100.0f);
+  lodNode->addChild(cessnaNodeLod3.get(), 100.0f, FLT_MAX);
+
+  if (cessnaNode) {
+         const osg::BoundingSphere& bs = cessnaNode->getBound();
+ 
+         float size = radius/bs.radius()*0.3f;
+         osg::MatrixTransform* positioned = new osg::MatrixTransform;
+         positioned->setDataVariance(osg::Object::STATIC);
+         positioned->setMatrix(osg::Matrix::translate(-bs.center())*
+                                      osg::Matrix::scale(size,size,size)*
+                                      osg::Matrix::rotate(osg::inDegrees(180.0f),0.0f,0.0f,1.0f));
+ 
+         positioned->addChild(lodNode.get());
+ 
+         cessnaXform = new osg::MatrixTransform;
+         cessnaXform->setUpdateCallback(new osg::AnimationPathCallback(animationPath,0.0f,2.0));
+         cessnaXform->addChild(positioned);
+ 
+  }
+
+  //Adding intersection functionality
+  osg::ref_ptr<osgUtil::IntersectorGroup> intersectorGroup = new osgUtil::IntersectorGroup();
+  osg::ref_ptr<osgUtil::LineSegmentIntersector> intersector = new osgUtil::LineSegmentIntersector(trip_p0, trip_p1);
+  intersectorGroup->addIntersector(intersector.get());
+
+  osgUtil::IntersectionVisitor intersectVisitor( intersectorGroup.get(), new MyReadCallback );
+
+  cessnaNode->accept(intersectVisitor);
+
+  if (intersector->containsIntersections()) {
+    std::cout << "hejsomfan" << std::endl;
+  }
+  else {
+    std::cout << "bajs" << std::endl;
+  }
+
+  /*osg::PositionAttitudeTransform* cessnaXform =
+   new osg::PositionAttitudeTransform();*/
+
+  osg::PositionAttitudeTransform* dumpTruckXform =
+   new osg::PositionAttitudeTransform();
+
+  // Declare and initialize a Vec3 instance to change the
+  // position of the cessna and dumptruck model in the scene
+  /*osg::Vec3 cessnaPos(60.0,0.0,0.0);
+  cessnaXform->setPosition(cessnaPos);*/
+
+  osg::Vec3 dumpTruckPos(-70,0.0,0.0);
+  dumpTruckXform->setPosition(dumpTruckPos);
 
   root->addChild(lineGeode);
   root->addChild(cessnaXform);
@@ -190,7 +292,6 @@ int main(int argc, char *argv[]){
 
   
   dumpTruckXform->addChild(dumpTruckNode);
-  cessnaXform->addChild(lodNode.get());
 
   // Optimizes the scene-graph
   //osgUtil::Optimizer optimizer;
@@ -205,6 +306,8 @@ int main(int argc, char *argv[]){
   camera->setViewMatrixAsLookAt (osg::Vec3d(0.0, 0.0, 2.0),
                                  osg::Vec3d(0.0, 0.0, 0.0),
                                  osg::Vec3d(0.0, 1.0, 0.0));
+
+  camera->setClearColor(osg::Vec4(0.3f, 0.3f, 0.3f, 1.0f));
   camera->getOrCreateStateSet()->setGlobalDefaults();
   viewer.setCamera(camera);
   
